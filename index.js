7a798476b2b0f4cc900c95cff2e4b90e5d83b8f6
eli456 <eli456pg@gmail.com>
2023-05-21 19:39:09 -0600

    inicializar code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Kafka } = require('kafkajs')
 const kafka = new Kafka({
   clientId: 'my-app',
   brokers: [
-	  'my-kafka-0.my-kafka-headless.kafka-adsoftsito.svc.cluster.local:9092'
+	  'my-kafka-0.my-kafka-headless.eli456.svc.cluster.local:9092'
 	  ]
 });
 
